619316e277556ed34cee200f2063f952cb6d41c1
VSmirnovaV <131758107+VSmirnovaV@users.noreply.github.com>
2023-10-22 14:56:48 +0300

    corrected task_1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,15 +4,16 @@ function solveEquation(a, b, c) {
 	let arr = [];
 	let d = Math.pow(b, 2) - 4 * a * c;
 	if (d < 0) {
-		arr = d;
+		arr;
 	}
 	if (d === 0) {
-		arr = -b / (2 * a);
+		let root = -b / (2 * a);
+		arr.push(root);
 	} else if (d > 0) {
-		let res = []
-		res.push((-b + Math.sqrt(d)) / (2 * a));
-		res.push((-b - Math.sqrt(d)) / (2 * a));
-		arr = res;
+		let root_1 = (-b + Math.sqrt(d)) / (2 * a);
+		let root_2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(root_1);
+		arr.push(root_2)
 	}
 	return arr;
 }

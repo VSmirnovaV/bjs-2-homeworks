949b69f4c9beead141bec5abbcd5f296e1688619
VSmirnovaV <131758107+VSmirnovaV@users.noreply.github.com>
2023-10-28 15:34:50 +0300

    task_1, task_2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.every((item, index) => item === arr2[index]) && arr1.length === arr2.length;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+  let result = users.filter(user => user.gender === gender).map(gender => gender.age).reduce((acc, item, index, arr)=> { acc+=item;
+  if (index === arr.length - 1) {
+    return acc / arr.length;
+  }
+  return acc;
+  }, 0)
+  return result;
+  }

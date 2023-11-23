72ec1afb8d233d741c8fc94b94c13139da51b1b2
VSmirnovaV <131758107+VSmirnovaV@users.noreply.github.com>
2023-11-23 20:55:21 +0300

    task_1,2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,49 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+	let cache = [];
+
+	function wrapper(...args) {
+		const hash = md5(args);
+		let objectInCache = cache.find((item) => item.hash === hash);
+		if (objectInCache) {
+			console.log("Из кеша: " + objectInCache.value);
+			return "Из кеша: " + objectInCache.value;
+		}
+
+		let result = func(...args);
+		cache.push({
+			hash,
+			value: result
+		});
+		if (cache.length > 5) {
+			cache.shift();
+		}
+		console.log("Вычисляем: " + result);
+		return "Вычисляем: " + result;
+	}
+	return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId = null;
+
+	function wrapper(...args) {
+		if (!timeoutId) {
+			func(...args);
+			timeoutId += delay;
+			wrapper.count += 1;
+		} else if (timeoutId) {
+			clearTimeout(timeoutId);
+			timeoutId = setTimeout(() => {
+				delay += timeoutId;
+				func(...args);
+				wrapper.count += 1;
+			}, delay);
+		}
+		wrapper.allCount += 1;
+	}
+	wrapper.allCount = 0;
+	wrapper.count = 0;
+	return wrapper;
 }
